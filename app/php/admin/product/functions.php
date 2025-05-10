ea535ee6dfcfe8dba69a763e4eee41f70ba69e5f
mustafa <mustafa.salah.2131@gmail.com>
2025-05-10 12:27:54 +0300

    feat(product): add functions for product management
    Introduce new functions to handle product-related operations, including fetching product count, retrieving all products, and fetching a product by its ID. These functions enhance the product management capabilities in the admin panel.

---
+++ app/php/admin/product/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function getAllProductCount()
+{
+    global $connect;
+    try {
+        $sql = "SELECT COUNT(*) AS number_of_product FROM products";
+        $stmt = $connect->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC)['number_of_product'];
+    } catch (PDOException $e) {
+        error_log("Error getting product count: " . $e->getMessage());
+        return false;
+    }
+}
+
+function getAllProducts()
+{
+    global $connect;
+    try {
+        $sql = "SELECT * FROM products";
+        $stmt = $connect->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log("Error getting all products: " . $e->getMessage());
+        return false;
+    }
+}
+
+function getProductById($productId)
+{
+    global $connect;
+    try {
+        $sql = "SELECT * FROM products WHERE id = :product_id";
+        $stmt = $connect->prepare($sql);
+        $stmt->bindParam(':product_id', $productId, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log("Error getting product by ID: " . $e->getMessage());
+        return false;
+    }
+}
