8b97ea0a35859ca16411decc2e3a8863aa0d0152
Mustafa Salah <mustafa.salah.2131@gmail.com>
2025-05-14 21:02:30 +0300

    feat(navbar): refactor navbar structure for improved responsiveness and styling

--- components/admin/app.php
+++ components/admin/app.php
@@ -24,19 +24,21 @@
 
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-5">
-    <a class="navbar-brand" href="dashboard.php">GameStore Admin</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse   justify-content-end" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <span class="nav-link">Welcome, <?php echo htmlspecialchars($_SESSION["username"]); ?></span>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="dashboard.php">GameStore Admin</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <span class="nav-link">Welcome, <?php echo htmlspecialchars($_SESSION["username"]); ?></span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="logout.php">Logout</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
